582628b971c8681efa4dc57cc5a22369020865f7
Sabir Hussain <email.sabirhussain@gmail.com>
2019-09-12 23:19:12 +0500

    initial commit

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
 const passport = require("passport");
-const passport2 = require("passport")
+const passport2 = require("passport");
 const admin = require("./routes/admin");
 const user = require("./routes/user");
-const cors = require("cors")
+const cors = require("cors");
+const path = require("path");
 
 // Bodyparser middleware
 app.use(
@@ -15,29 +16,34 @@ app.use(
   })
 );
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
 // Connect to MongoDB
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
+  .connect(process.env.MONGODB_URL || db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
 
 // Passport middleware
-app.use(passport.initialize())
-app.use(passport2.initialize())
+app.use(passport.initialize());
+app.use(passport2.initialize());
 
 // Passport config
-require("./config/passport")(passport)
-require("./config/passport2")
+require("./config/passport")(passport);
+require("./config/passport2");
+
+// Server configurations
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("cliend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "cliend", "build", "index.html"));
+  });
+}
 
 // Routes
 app.use("/", admin);
-app.use("/", user)
+app.use("/", user);
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
